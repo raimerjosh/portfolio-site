6e6c447167b072109a5fbc884038a416c2530cd2
Joshua Raimer <joshraimer@gmail.com>
2023-08-07 14:28:58 -0500

    added and updated portfolio section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,10 +8,10 @@ export default function Footer(){
 
       <ul className='permalinks'>
         <li><a href='#'>Home</a></li>
-        <li><a href='about'>About</a></li>
-        <li><a href='experience'>Experience</a></li>
-        <li><a href='portfolio'>Portfolio</a></li>
-        <li><a href='contact'>Contact</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#contact'>Contact</a></li>
       </ul>
 
       <div className="footer_socials">
--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import "./portfolio.css";
-import IMG1 from "../../assests/portfolio_1.webp";
-import IMG2 from "../../assests/portfolio_2.webp";
-import IMG3 from "../../assests/portfolio_3.webp";
+import IMG1 from "../../assests/portfolio_1.png";
+import IMG2 from "../../assests/portfolio_2.png";
+import IMG3 from "../../assests/portfolio_3.png";
 import IMG4 from "../../assests/portfolio_4.webp";
 
 export default function Portfolio(){
@@ -16,10 +16,9 @@ export default function Portfolio(){
           <div className="portfolio_item-image">
             <img src={IMG1}/>
           </div>
-          <h3>This is a portfolio item</h3>
+          <h3>Plant Store Ecommerce</h3>
           <div className='portfolio_item-cta'>
-            <a href="https://github.com/raimerjosh" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com/raimerjosh" className='btn btn-primary' target='_blank'>Live Demo</a>
+            <a href="https://github.com/raimerjosh/ecommerce" className='btn' target='_blank'>Github</a>
           </div>
         </article>
 
@@ -27,10 +26,9 @@ export default function Portfolio(){
           <div className="portfolio_item-image">
             <img src={IMG2}/>
           </div>
-          <h3>This is a portfolio item</h3>
+          <h3>Golf Score Database</h3>
           <div className='portfolio_item-cta'>
-            <a href="https://github.com/raimerjosh" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com/raimerjosh" className='btn btn-primary' target='_blank'>Live Demo</a>
+            <a href="https://github.com/raimerjosh/pern-app" className='btn' target='_blank'>Github</a>
           </div>
         </article>
 
@@ -38,10 +36,9 @@ export default function Portfolio(){
           <div className="portfolio_item-image">
             <img src={IMG3}/>
           </div>
-          <h3>This is a portfolio item</h3>
+          <h3>React & AWS Hotel</h3>
           <div className='portfolio_item-cta'>
-            <a href="https://github.com/raimerjosh" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com/raimerjosh" className='btn btn-primary' target='_blank'>Live Demo</a>
+            <a href="https://github.com/raimerjosh/react-aws" className='btn' target='_blank'>Github</a>
           </div>
         </article>
 
@@ -52,7 +49,7 @@ export default function Portfolio(){
           <h3>This is a portfolio item</h3>
           <div className='portfolio_item-cta'>
             <a href="https://github.com/raimerjosh" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com/raimerjosh" className='btn btn-primary' target='_blank'>Live Demo</a>
+            {/* <a href="https://github.com/raimerjosh" className='btn btn-primary' target='_blank'>Live Demo</a> */}
           </div>
           
         </article>
